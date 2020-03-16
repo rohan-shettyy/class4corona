f8849d0cf2aa7751be6650dc625762c4272566c1
Wilson (Fengchi) Wang <wilsonfwang@gmail.com>
2020-03-15 20:01:19 -0400

    Not Done Socket Chat implementation!

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 var express = require('express');
+var SocketIO = require("socket.io")
 var app = express();
 var server = require('http').Server(app);
 var fs = require('fs');
 var path = require('path');
 var cors = require('cors');
+var io = SocketIO(server)
 
+
+var users = {};
+var name = '';
+
+app.get('/:name', function(req,res){
+	name = req.param.name;
+	res.sendFile(path.join(__dirname, "/public/classStudent.html"))
+});
+
+io.sockets.on("connection", function(socket){
+	users[socket.id] = name;
+
+		socket.on("nRoom", function(room){
+			socket.join(room);
+			socket.broadcast.in(room).emit("node new user", users[socket.id] + " new user has joined");
+		});
+
+		socket.on("node new message", function(data){
+			io.sockets.in("nRoom").emit('node news', users[socket.id] + ": "+ data);
+		});
+})
 
 app.use(cors());
 app.use(express.urlencoded( {extended: true} ))
