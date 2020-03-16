0d9bd4510a83b7d3fc1fa7cb672315a170b68dcf
Wilson (Fengchi) Wang <wilsonfwang@gmail.com>
2020-03-16 16:32:15 -0400

    Quick Fix for the routing

--- main.js
+++ main.js
@@ -11,24 +11,6 @@ var io = SocketIO(server)
 var users = {};
 var name = '';
 
-app.get('/:name', function(req,res){
-	name = req.param.name;
-	res.sendFile(path.join(__dirname, "/public/classStudent.html"))
-});
-
-io.sockets.on("connection", function(socket){
-	users[socket.id] = name;
-
-		socket.on("nRoom", function(room){
-			socket.join(room);
-			socket.broadcast.in(room).emit("node new user", users[socket.id] + " new user has joined");
-		});
-
-		socket.on("node new message", function(data){
-			io.sockets.in("nRoom").emit('node news', users[socket.id] + ": "+ data);
-		});
-})
-
 app.use(cors());
 app.use(express.urlencoded( {extended: true} ))
 // Use correct file directory
