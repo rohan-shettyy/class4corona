cdbec8a23a56be1091a7744aa27757e8eb0d08f9
Wilson (Fengchi) Wang <wilsonfwang@gmail.com>
2020-03-31 16:09:25 -0400

    (Update) : Removed favicon stuff and updated the host setup page

--- public/js/setuphost.js
+++ public/js/setuphost.js
@@ -1,12 +1,20 @@
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('session');
+var localStream;
 
-navigator.mediaDevices.getUserMedia({
-    audio: true
-}).then(function(stream) {
-    localStream = stream
-}).catch(function(err) {
-    alert("Microphone Permissions are needed")
+$(document).ready(function() {
+    var video = document.querySelector("#localVideo");
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+            .then(function(stream) {
+                video.srcObject = stream;
+            })
+            .catch(function(err) {
+                console.log("Something went wrong!");
+            });
+    }
+
 });
 
 function host() {
